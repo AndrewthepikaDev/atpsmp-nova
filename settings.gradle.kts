b54634a23b6514921e8a24e0988a1a9e6a4d3feb
Andrewthepika_Dev <105611457+AndrewthepikaDev@users.noreply.github.com>
2026-06-01 09:48:59 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "example" // TODO: Change this to your addon id
+rootProject.name = "atpsmp" // TODO: Change this to your addon id
 
 dependencyResolutionManagement {
     repositories {
